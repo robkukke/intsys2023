3b58f7e89575d0182464162f6388a485fb2f4374
Robin Kukke <robkukke@users.noreply.github.com>
2023-09-20 20:16:21 +0300

    02_trips_traps_trull

--- 02_trips_traps_trull/script.js
+++ 02_trips_traps_trull/script.js
@@ -4,61 +4,100 @@
    st. alakriips tähistab tühja ruutu, O või X ruudus olevat sümbolit.
    Viimane (kümnes) sümbol näitab, kelle kord on käia. */
 
-let initialState = "_________X";
-let currentState = initialState;
+let currentGameState = "XO_OX_X__X";
 const currentLayer = document.getElementById("currentLayer");
 const selectionLayer = document.getElementById("selectionLayer");
+const treeLayer = document.getElementById("treeLayer");
 
 document.addEventListener("DOMContentLoaded", () => {
-    displayStates();
+    displayGameState();
 });
 
-function getSubStates(state) {
+function getValidNextStates(state) {
     const player = state.slice(-1);
     const opponent = (player === "X") ? "O" : "X";
-    const subStates = [];
+    const validNextStates = [];
 
     for (let space = 0; space < 9; space++) {
         if (state[space] === "_") {
-            const subState = state.slice(0, space) + player + state.slice(space + 1, 9) + opponent;
+            const nextState = state.slice(0, space) + player + state.slice(space + 1, 9) + opponent;
 
-            subStates.push(subState);
+            validNextStates.push(nextState);
         }
     }
 
-    return subStates;
+    return validNextStates;
 }
 
 function stateToString(state) {
     return state.match(/.{3}/g).join("<br>");
 }
 
-function displayStates() {
-    currentLayer.innerHTML = stateToString(currentState);
+function checkForWinner(state) {
+    const winningCombos = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]];
 
-    displaySubStates();
+    for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+
+        if (state[a] === state[b] && state[b] === state[c] && state[a] !== '_') {
+            return state[a];
+        }
+    }
+
+    return false;
 }
 
-function displaySubStates() {
-    const subStates = getSubStates(currentState).map(subState => {
-        return `${stateToString(subState)} <br><br> <input type='button' value='Vali' data-substate='${subState}' />`;
+function generateStateTree(state, isTopNode = true) {
+    const winner = checkForWinner(state);
+    const validNextStates = getValidNextStates(state);
+
+    if (isTopNode) {
+        return `<ul>${validNextStates.map(nextState => generateStateTree(nextState, false)).join('')}</ul>`;
+    }
+
+    if (winner) {
+        return `<li>${stateToString(state)}<br>Võitis ${winner}</li>`;
+    }
+
+    if (validNextStates.length === 0) {
+        return `<li>${stateToString(state)}<br>Viik</li>`;
+    }
+
+    return `<li>${stateToString(state)}<ul>${validNextStates.map(nextState => generateStateTree(nextState, false))
+        .join('')}</ul></li>`;
+}
+
+function displayGameState() {
+    currentLayer.innerHTML = stateToString(currentGameState);
+
+    displayValidNextStates();
+    displayStateTree();
+}
+
+function displayValidNextStates() {
+    const validNextStates = getValidNextStates(currentGameState).map(nextState => {
+        return `${stateToString(nextState)}<br><br><input type='button' value='Vali' data-nextstate='${nextState}' />`;
     });
 
-    selectionLayer.innerHTML = subStates.join("<br><br>");
+    selectionLayer.innerHTML = validNextStates.join("<br><br>");
 
     const buttons = selectionLayer.querySelectorAll("input[type='button']");
 
     buttons.forEach(button => {
         button.addEventListener("click", () => {
-            const subState = button.getAttribute("data-substate");
+            const nextState = button.getAttribute("data-nextstate");
 
-            selectState(subState);
+            selectState(nextState);
         });
     });
 }
 
-function selectState(newState) {
-    currentState = newState;
+function displayStateTree() {
+    treeLayer.innerHTML = generateStateTree(currentGameState);
+}
 
-    displayStates();
+function selectState(newState) {
+    currentGameState = newState;
+
+    displayGameState();
 }
